4109dd4432b42304b31981b75df9558f81dcc5d0
Rahman Usta <rahmanusta@kodedu.com>
2021-03-30 22:34:14 +0200

    compact constructor

--- src/main/java/com/kodedu/records/Point.java
+++ src/main/java/com/kodedu/records/Point.java
@@ -2,12 +2,10 @@ package com.kodedu.records;
 
 public record Point(int x, int y) {
 
-    public Point(int x, int y) {
+    public Point {
         if (x < 0 || y < 0) {
             throw new IllegalArgumentException("X or Y cannot be negative");
         }
-        this.x = x;
-        this.y = y;
     }
 
 }
