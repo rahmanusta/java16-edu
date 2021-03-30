874755937aa3d17ef93546eacf9d04cf5bacca54
Rahman Usta <rahmanusta@kodedu.com>
2021-03-30 22:35:16 +0200

    compact constructor

--- src/main/java/com/kodedu/records/Point.java
+++ src/main/java/com/kodedu/records/Point.java
@@ -2,6 +2,8 @@ package com.kodedu.records;
 
 public record Point(int x, int y) {
 
+//    you can declare constructor or other methods
+//    if you want to override the default behaviour
     public Point {
         if (x < 0 || y < 0) {
             throw new IllegalArgumentException("X or Y cannot be negative");
